02d7f5efcabd08fbfbcf64d7c88eab4639ec6d62
Mirko1523 <134454433+Mirko1523@users.noreply.github.com>
2025-01-27 18:48:38 -0300

    feat: profile with public properties

---
+++ classes/Profile.php
@@ -0,0 +1,24 @@
+<?php  
+
+//! definimos el strict types para verificar que los datos que pongamos osn los correctos
+declare(strict_types=1);
+
+//! declaramos una clase en donde se representará el perfil del usuario generado con sus propiedades y metodos
+class Profile {
+    public string $name;
+    public int $age;
+    public string $language; 
+
+// !trabajaré con __construct para definir la clase.
+public function __construct(string $name, int $age, string $language) {
+    //!asignamos el valor a los diferentes parametros (name, age y languaje)
+  $this -> name = $name;
+  $this -> age = $age;
+  $this -> language = $language;
+}
+
+//!con esta funcion devolvemos una cadena de texto(string) que seria el perfil generado mostrando los diferentes valores.
+public function textUser(): string{
+  return "Nombre: $this->name | Edad: $this->age | Lenguaje Favorito: $this->language";
+}
+};
